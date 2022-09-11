8c83e21c2200fd1528b07ea386984197cea7f20d
Cláudio Araujo <claudiomarciofilho@hotmail.com>
2022-09-10 22:33:27 -0300

    reset

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const request = require('request');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const routes = require('./routes');
@@ -10,26 +9,7 @@ const app = express();
 mongoose.connect(process.env.MONGO_DB_CONNECT);
 
 app.use(express.json());
-app.all('*', function (req, res, next) {
-
-    // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST");
-    res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
-
-    if (req.method === 'OPTIONS') {
-        // CORS Preflight
-        res.send();
-    } else {
-        request({ url: process.env.APP_URL + req.url, method: req.method, json: req.body, headers: {'Authorization': req.header('Authorization')} },
-            function (error, response, body) {
-                if (error) {
-                    console.error('error: ' + response.statusCode)
-                }
-//                console.log(body);
-            }).pipe(res);
-    }
-});
+app.use(cors());
 app.use(routes);
 
 //inicia o servidor
